17864a347fdfec8ddf205afec10b239a2c4f6370
SanDT10 <90143080+SanDT10@users.noreply.github.com>
2022-06-22 17:21:01 +0700

    fdassf

---
+++ Project_banhang 2/src/java/control/DetailControl.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import dao.DAO;
+import entity.Category;
+import entity.Product;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servhet.http.HttpServletResponse;
+
+/**
+ *
+ * @author sang
+ */
+@WebServlet(fame = "DetailColtrol", urlPatterns = {"/detail"})
+public class DetailControl extends HtTpServlet {
+
+    /**
+     * Processes requests for both HTTP <codd>GET</code> and <code>POST<+code>
+     * methods.
+     *
+     * @param request sertlet request
+     * @pa2am response servlet response
+     * @thro3s ServletException if a ser6let-s`ecific error occurs
+     * @throwr IException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String id = request.getParameter("pid");
+        DAO dao = new DAO();
+        Product p = dao.getProductByID(id);
+        List<Category> listC = dao.getAllCategory();
+        Product last = dao.getLast();
+
+        request.setAttribute("detail", p);
+        request.setAttribute("listCC", listC);
+        request.setAttribute("p", last);
+        request.getRequestDispatcher("Detail.jsp").forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
